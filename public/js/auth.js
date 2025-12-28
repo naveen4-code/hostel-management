e98b59a5970e9d8a6c69184591929e9347f987db
Naveen Raj K <knaveenraj725@gmail.com>
2025-12-28 23:40:29 +0530

    Updated the file with new changes

--- public/js/auth.js
+++ public/js/auth.js
@@ -8,13 +8,22 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 /* 👇 VERY IMPORTANT: attach to window */
-window.login = async function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+window.login = async () => {
+  try {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-  const res = await signInWithEmailAndPassword(auth, email, password);
-  redirect(res.user.uid);
+    const res = await signInWithEmailAndPassword(auth, email, password);
+    redirect(res.user.uid);
+  } catch (error) {
+    if (error.code === "auth/invalid-credential") {
+      alert("Invalid email or password");
+    } else {
+      alert(error.message);
+    }
+  }
 };
+
 
 window.googleLogin = async function () {
   const res = await signInWithPopup(auth, provider);
